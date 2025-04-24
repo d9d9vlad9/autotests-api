5341307e15461cfb27183da1289c9a301a992396
d9d9_vlad9 <dopgor555@gmail.com>
2025-04-24 22:37:18 +0500

    Поправил форматы строк, для соответсвия стандартам PEP8

--- grpc_course_server.py
+++ grpc_course_server.py
@@ -5,7 +5,11 @@ import course_service_pb2_grpc
 
 class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
     def GetCourse(self, request, context):
-        return course_service_pb2.GetCourseResponse(course_id=request.course_id, title="Автотесты API", description="Будем изучать написание API автотестов")
+        return course_service_pb2.GetCourseResponse(
+            course_id=request.course_id,
+            title="Автотесты API",
+            description="Будем изучать написание API автотестов"
+        )
 
 def serve():
     """Функция создает и запускает gRPC-сервер"""
