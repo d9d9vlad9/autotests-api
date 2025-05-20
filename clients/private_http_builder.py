67c27b03b326ffe94ca9043610f9e515e9f4a2a6
d9d9_vlad9 <dopgor555@gmail.com>
2025-05-20 23:00:15 +0500

    Рефакторинг и подготовка к написанию API автотестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -2,15 +2,17 @@ from httpx import Client
 from pydantic import BaseModel, EmailStr
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
+from functools import lru_cache
 
 
-class AuthenticationUserSchema(BaseModel):
+class AuthenticationUserSchema(BaseModel, frozen=True):
     """
     Тип данных для пользователя аутентификации.
     """
     email: EmailStr
     password: str
 
+@lru_cache(maxsize=None)
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     """
     Создает и возвращает экземпляр httpx.Client с настройками для приватного HTTP-клиента.
---
+++ tests/users/test_users.py
@@ -0,0 +1,37 @@
+import pytest
+from http import HTTPStatus
+from clients.users.private_users_client import PrivateUsersClient
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
+from tools.fakers import fake
+
+
+@pytest.mark.users
+@pytest.mark.regression
+class TestUsers:
+    @pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"], ids=lambda email: f"email: {email}")
+    def test_create_user(self, email: str, public_users_client: PublicUsersClient):
+        request = CreateUserRequestSchema(email=fake.email(domain=email))
+        response = public_users_client.create_user_api(request)
+        response_data = CreateUserResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_user_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_me(
+            self,
+            private_users_client: PrivateUsersClient,
+            function_user
+    ):
+        response = private_users_client.get_users_me_api()
+        response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+        assert_get_user_response(response_data, function_user.response.user)
