1d2de3066b1340fc1bba670a1f281bed8a15d80e
d9d9_vlad9 <dopgor555@gmail.com>
2025-05-22 00:05:18 +0500

    Практикуемся в написании API автотестов. Часть 1. Практика написания негативных API автотестов для работы с файлами

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -94,3 +94,27 @@ def assert_file_not_found_response(actual: InternalErrorResponseSchema):
     expected = InternalErrorResponseSchema(details="File not found")
     # Используем ранее созданную функцию для проверки внутренней ошибки
     assert_internal_error_response(actual, expected)
+
+def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ на получение файла с некорректным идентификатором файла соответствует ожидаемой валидационной ошибке.
+    :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
+    :raises: AssertionError: Если фактический ответ не соответствует ошибке "uuid_parsing"
+    """
+    expected = ValidationErrorResponseSchema(
+            detail= [
+                ValidationErrorSchema(
+                    type = "uuid_parsing",
+                    loc= [
+                        "path",
+                        "file_id"
+                    ],
+                    msg= "Input should be a valid UUID, invalid character: expected an optional prefix of `urn:uuid:` followed by [0-9a-fA-F-], found `i` at 1",
+                    input= "incorrect-file-id",
+                    ctx= {
+                        "error": "invalid character: expected an optional prefix of `urn:uuid:` followed by [0-9a-fA-F-], found `i` at 1"
+                    }
+                )
+            ]
+    )
+    assert_validation_error_response(actual, expected)
