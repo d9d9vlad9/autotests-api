c43aadb4a509711e8c4b19bf60be06e52874b69c
d9d9_vlad9 <dopgor555@gmail.com>
2025-05-24 02:38:11 +0500

    9.3 Практикуемся в написании API автотестов. Часть 2

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -38,6 +38,9 @@ class GetCoursesQuerySchema(BaseModel):
 
     user_id: str = Field(alias="userId")
 
+class GetCoursesResponseSchema(BaseModel):
+    courses: list[CourseSchema]
+
 class CreateCourseRequestSchema(Course):
     """
     Тип данных для запроса на создание курса.
@@ -54,3 +57,9 @@ class UpdateCourseRequestSchema(Course):
     min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
     description: str | None = Field(default_factory=fake.text)
     estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+class UpdateCourseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления курса.
+    """
+    course: CourseSchema
