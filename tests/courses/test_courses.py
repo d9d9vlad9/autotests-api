abcfcc5b1263556ab4716f53102e14bba34535b6
d9d9_vlad9 <dopgor555@gmail.com>
2025-05-24 18:22:38 +0500

    Правки после ревью

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -54,7 +54,10 @@ class TestCourses:
         """
         Проверяет создание курса.
         """
-        request = function_course.request
+        request = CreateCourseRequestSchema(
+            preview_file_id=function_file.response.file.id,
+            created_by_user_id=function_user.response.user.id
+        )
         response = courses_client.create_course_api(request)
         response_data = CreateCourseResponseSchema.model_validate_json(response.text)
 
